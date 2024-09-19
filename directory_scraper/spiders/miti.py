dde11b088d3d3959169262af8ce54f053204a09c
Asyiqin Nasir <asyiq@MacBook-Pro.local>
2024-09-19 13:29:41 +0800

    fix: miti.py - added function to switch language BM/EN

--- directory_scraper/spiders/miti.py
+++ directory_scraper/spiders/miti.py
@@ -5,6 +5,20 @@ class MitiSpider(scrapy.Spider):
     allowed_domains = ['www.miti.gov.my']
     start_urls = ['https://www.miti.gov.my/index.php/edirectory/edirectory_list/4']
 
+    def start_requests(self):
+        #1: switch language to BM
+        yield scrapy.Request(
+            url='https://www.miti.gov.my/index.php/multilingual/switch_language/BM', #can switch to EN
+            callback=self.after_language_switch
+        )
+
+    def after_language_switch(self, response):
+        #2: then, proceed to scrape the directory page
+        yield scrapy.Request(
+            url='https://www.miti.gov.my/index.php/edirectory/edirectory_list/4',
+            callback=self.parse
+        )
+
     def parse(self, response):
         csrf_token = response.css('input[name="ci_csrf_token"]::attr(value)').get() #csrf_token is in static html
 
@@ -17,6 +31,7 @@ class MitiSpider(scrapy.Spider):
             'keyword': 'All'
         }
 
+        # Send a POST request with the form data
         yield scrapy.FormRequest(
             url='https://www.miti.gov.my/index.php/edirectory/filter_search',
             formdata=payload,
@@ -40,7 +55,7 @@ class MitiSpider(scrapy.Spider):
     def parse_results(self, response):
         person_items = response.css('div.list-item')
 
-        for item in person_items: #default None if not available
+        for item in person_items:
             person_name = item.css('h1 a::text').get(default='').strip()
             person_position = item.css('p::text').get(default='').strip()
             profile_url = item.css('h1 a::attr(href)').get(default='')
@@ -54,8 +69,6 @@ class MitiSpider(scrapy.Spider):
 
             if person_email:
                 person_email = person_email + '@miti.gov.my'
-
-            #print(f"Scraping: {person_name} ") #logging
 
             yield {
                 'agency': "KEMENTERIAN PELABURAN, PERDAGANGAN DAN INDUSTRI MALAYSIA",
@@ -74,5 +87,5 @@ class MitiSpider(scrapy.Spider):
         if index < len(details):
             return details[index].css('td.data-label::text').get(default='').strip()
         return None
-    
-#language EN
+
+#language EN/BM
