8d44c4a6c0e42a189c67962a1e976bfcc2272012
AbdulHafiiz <arhafiiz@gmail.com>
2024-09-23 14:24:06 +0800

    added in  and  to output

--- directory_scraper/spiders/motac.py
+++ directory_scraper/spiders/motac.py
@@ -13,6 +13,50 @@ class MOTACSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow="direktori"), callback='parse_item'),
     )
+    
+    url_lst = [
+        "https://www.motac.gov.my/direktori/menteri",
+        "https://www.motac.gov.my/direktori/timbalan-menteri",
+        "https://www.motac.gov.my/direktori/ksu",
+        "https://www.motac.gov.my/direktori/tksupl",
+        "https://www.motac.gov.my/direktori/tksuk",
+        "https://www.motac.gov.my/direktori/pejabat-timbalan-ketua-setiausaha-pengurusan",
+        "https://www.motac.gov.my/direktori/dpl",
+        "https://www.motac.gov.my/direktori/dk",
+        "https://www.motac.gov.my/direktori/hak",
+        "https://www.motac.gov.my/direktori/pi",
+        "https://www.motac.gov.my/direktori/pa",
+        "https://www.motac.gov.my/direktori/kewangan",
+        "https://www.motac.gov.my/direktori/pentadbiran",
+        "https://www.motac.gov.my/direktori/akaun",
+        "https://www.motac.gov.my/direktori/bppp",
+        "https://www.motac.gov.my/direktori/bpm",
+        "https://www.motac.gov.my/direktori/psm",
+        "https://www.motac.gov.my/direktori/pp",
+        "https://www.motac.gov.my/direktori/matic",
+        "https://www.motac.gov.my/direktori/ukk",
+        "https://www.motac.gov.my/direktori/puu",
+        "https://www.motac.gov.my/direktori/uad",
+        "https://www.motac.gov.my/direktori/ui",
+        "https://www.motac.gov.my/direktori/kpi",
+        "https://www.motac.gov.my/direktori/vm2026",
+        "https://www.motac.gov.my/direktori/mm2h",
+        "https://www.motac.gov.my/direktori/motac-perlis",
+        "https://www.motac.gov.my/direktori/motac-kedah",
+        "https://www.motac.gov.my/direktori/motac-penang",
+        "https://www.motac.gov.my/direktori/motac-perak",
+        "https://www.motac.gov.my/direktori/motac-selangor",
+        "https://www.motac.gov.my/direktori/motac-melaka",
+        "https://www.motac.gov.my/direktori/motac-negeri-sembilan",
+        "https://www.motac.gov.my/direktori/motac-johor",
+        "https://www.motac.gov.my/direktori/motac-kelantan",
+        "https://www.motac.gov.my/direktori/motac-terengganu",
+        "https://www.motac.gov.my/direktori/motac-pahang",
+        "https://www.motac.gov.my/direktori/motac-sarawak",
+        "https://www.motac.gov.my/direktori/motac-sabah",
+        "https://www.motac.gov.my/direktori/motac-kl-putrajaya",
+        "https://www.motac.gov.my/direktori/motac-labuan"
+    ]
 
     def parse_item(self, response):
         division = response.css("h1::text").get()
@@ -23,21 +67,23 @@ class MOTACSpider(CrawlSpider):
             for person_data in response.css("td[class='uk-width-7-10']").getall()
         ]
 
-        for datapoint in directory_data:
+        for person_order, datapoint in enumerate(directory_data):
             name = datapoint[0].strip()
             position = datapoint[1].strip()
             unit = datapoint[2].strip() if len(datapoint) > 3 else None
-            email = email_str[0].strip() if (email_str := re.sub(pattern=phone_regex, repl="", string=datapoint[-1])) else None
+            email = email_str.strip() if (email_str := re.sub(pattern=phone_regex, repl="", string=datapoint[-1])) else None
             phone = phone_str[0].strip() if (phone_str := re.findall(pattern=phone_regex, string=datapoint[-1])) else None
 
             person_data = {
                 "agency": "KEMENTERIAN PELANCONGAN, SENI DAN BUDAYA",
                 "person_name": name,
-                "person_email": email,
+                "person_sort_order": person_order+1,
+                "person_email": email+"@motac.gov.my",
                 "person_phone": phone,
                 "person_position": position,
                 "unit": unit,
-                "division": division
+                "division": division,
+                "division_sort_order": self.url_lst.index(response.url)+1
             }
 
             yield person_data
