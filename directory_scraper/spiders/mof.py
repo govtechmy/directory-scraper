d24fff278544552595be920279a54af3f6eb641c
Asyiqin Nasir <asyiq@MacBook-Pro.local>
2024-09-24 11:22:10 +0800

    feat: mof.py added division and person sort order

--- directory_scraper/spiders/mof.py
+++ directory_scraper/spiders/mof.py
@@ -3,6 +3,36 @@ import re
 
 class MOFSpider(scrapy.Spider):
     name = 'mof'
+
+    #manual mapping of division names to sort order
+    division_sort_mapping = {
+        "Pejabat Menteri, Timbalan Menteri dan Pegawai Atasan": 1,
+        "Bahagian Antarabangsa (INT)": 2,
+        "Bahagian Cukai (TAX)": 3,
+        "Bahagian Dasar Saraan dan Pengurusan (RPM)": 4,
+        "Bahagian Fiskal dan Ekonomi (FED)": 5,
+        "Bahagian Pelaburan Strategik (SID)": 6,
+        "Bahagian Pengurusan Aset Awam (PAM)": 7,
+        "Bahagian Pengurusan Strategik Badan Berkanun (SBM)": 8,
+        "Bahagian Perolehan Kerajaan (GPD)": 9,
+        "Bahagian Kawalan Kewangan Strategik dan Korporat (BKSK)": 10,
+        "Bahagian Syarikat Pelaburan Kerajaan (GIC)": 11,
+        "Bahagian Teknologi Maklumat (ITD)": 12,
+        "Bahagian Undang-Undang (BUU)": 13,
+        "Pejabat Belanjawan Negara (NBO)": 14,
+        "Pejabat Pendaftar Agensi Pelaporan Kredit (PPK)": 15,
+        "Pejabat Pesuruhjaya Khas Cukai Pendapatan (PKCP)": 16,
+        "Tribunal Rayuan Kastam (TRK)": 17,
+        "Unit Audit Dalam Perbendaharaan (ADP)": 18,
+        "Unit Integriti (UI)": 19,
+        "Unit Pantau Madani": 20,
+        "Unit Komunikasi Korporat (UKK)": 21,
+        #"Perbendaharaan Malaysia Sabah": 22, #website is different
+        #"Perbendaharaan Malaysia Sarawak":23, #website is different
+    }
+
+    person_sort_order = 0
+
     start_urls = [
         'https://www.mof.gov.my/portal/ms/hubungi/direktori/menteri-dan-pegawai-atasan',
         'https://www.mof.gov.my/portal/index.php/hubungi/direktori/int',
@@ -24,7 +54,9 @@ class MOFSpider(scrapy.Spider):
         'https://www.mof.gov.my/portal/ms/hubungi/direktori/adp',
         'https://www.mof.gov.my/portal/ms/hubungi/direktori/ui',
         'https://www.mof.gov.my/portal/ms/hubungi/direktori/madani',
-        'https://www.mof.gov.my/portal/ms/hubungi/direktori/unit-komunikasi-korporat'
+        'https://www.mof.gov.my/portal/ms/hubungi/direktori/unit-komunikasi-korporat',
+        #'https://pmsabah.treasury.gov.my/index.php/ms/hubungi-kami', #website is different
+        #'http://pmsarawak.treasury.gov.my/?page_id=46' #website is different
     ]
 
     def parse(self, response):
@@ -48,6 +80,9 @@ class MOFSpider(scrapy.Spider):
         unit_address = None  # initialize
         unit_phone = None  # initialize
 
+        #determine the division sort order using the mapping
+        division_sort_order = self.division_sort_mapping.get(division, 999)  #use 999 as default if not found
+
         #iterate row
         rows = response.css('table.table tbody tr')
         for row in rows:
@@ -63,6 +98,8 @@ class MOFSpider(scrapy.Spider):
                 
                 continue  #move to the next row after setting the "unit" value
 
+            self.person_sort_order += 1
+
             person_name = row.css('td strong::text').get(default='').strip()
             person_position = row.css('td::text').getall()[1].strip() if len(row.css('td::text').getall()) > 1 else ''
             
@@ -71,15 +108,16 @@ class MOFSpider(scrapy.Spider):
 
             yield {
                 'agency': agency,
+                'division_sort_order': division_sort_order,
+                'person_sort_order': self.person_sort_order,
                 'division': division,
                 #'division_address': division_address,
                 #'division_phone': division_phone,
                 #'division_fax': division_fax,
-                'unit': current_unit,  
-                #'unit_address': unit_address if unit_address else None,  #store None if missing
-                #'unit_phone': unit_phone if unit_phone else None,
+                'unit': current_unit,
+                #'unit_address': unit_address if unit_address else None,
                 'person_name': person_name,
-                'person_phone': person_phone if person_phone else None,  #store None if missing
+                'person_phone': person_phone if person_phone else None,
                 'person_position': person_position,
-                'person_email': person_email if person_email else None  #store None if missing
-                }
+                'person_email': person_email if person_email else None
+            }
