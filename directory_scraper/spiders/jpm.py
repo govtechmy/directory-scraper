f1d0202bc76900df1b321982fdee3845fa4c7c4c
Asyiqin Nasir <asyiq@MacBook-Pro.local>
2024-09-24 17:00:16 +0800

    fix: fix jpm.py unit parsing issues - multiple scenario

--- directory_scraper/spiders/jpm.py
+++ directory_scraper/spiders/jpm.py
@@ -36,7 +36,26 @@ class JPMSpider(scrapy.Spider):
         accordion_sections = response.css('div.card')
 
         for section in accordion_sections:
-            unit = section.css('div.card-header h3::text').get(default='').strip()
+
+            main_unit_case1 = section.css('div.card-header h2::text').get(default='').strip()
+            main_unit_case2 = section.css('div.card-header h2 b::text').get(default='').strip() #jpm11
+
+            detailed_unit_case1 = section.css('div.card-header h3::text').get(default='').strip() #jpm5, jpm8, jpm2
+            detailed_unit_case2 = section.css('div.card-header2 h4 b::text').get(default='').strip()
+
+            if main_unit_case1:
+                main_unit = main_unit_case1
+            else:
+                main_unit = main_unit_case2
+
+            if detailed_unit_case1:
+                detailed_unit = detailed_unit_case1
+            else:
+                detailed_unit = detailed_unit_case2
+
+            if not main_unit and detailed_unit:
+                main_unit = detailed_unit
+                detailed_unit = None
 
             #iterate contact row in the section's table
             for contact in section.css('tbody tr.d-flex'):
@@ -52,13 +71,20 @@ class JPMSpider(scrapy.Spider):
                     person_email_prefix = ''     
                 person_email = f"{person_email_prefix}@jpm.gov.my" if person_email_prefix else ''
 
+                # Handle cases where there might be only main_unit, only detailed_unit, both, or neither
+                unit_full = None
+                if main_unit and detailed_unit:
+                    unit_full = f"{main_unit} > {detailed_unit}"  # Combine main unit and detailed unit
+                elif main_unit:
+                    unit_full = main_unit  # Only main unit exists
+
                 yield {
                     'agency': agency,
                     'division': division if division else None,
                     #'division_address': division_address,
                     #'division_phone': division_phone,
                     'person_name': person_name if person_name else None,
-                    'unit': unit if unit else None,
+                    'unit': unit_full if unit_full else None,
                     'person_phone': person_phone if person_phone else None,
                     'person_position': person_position if person_position else None,
                     'person_email': person_email if person_email else None,
