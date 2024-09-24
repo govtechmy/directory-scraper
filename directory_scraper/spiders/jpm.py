f9b84936c026822c8720b6b6de1ef59ebbc8aa11
Asyiqin Nasir <asyiq@MacBook-Pro.local>
2024-09-24 17:59:56 +0800

    fix: fix jpm.py missing unit

--- directory_scraper/spiders/jpm.py
+++ directory_scraper/spiders/jpm.py
@@ -26,7 +26,6 @@ class JPMSpider(scrapy.Spider):
             )
 
     def parse_static(self, response):
-        agency = "JABATAN PERDANA MENTERI"
 
         division = response.css('h3.card-title::text').get(default='').strip()
         division_address = response.css('span.text-sm.text.mb-0::text').get(default='').strip()
@@ -37,25 +36,38 @@ class JPMSpider(scrapy.Spider):
 
         for section in accordion_sections:
 
-            main_unit_case1 = section.css('div.card-header h2::text').get(default='').strip()
+            main_unit_case1 = section.css('div.card-header h2::text').get(default='').strip() #most jpm
             main_unit_case2 = section.css('div.card-header h2 b::text').get(default='').strip() #jpm11
 
-            detailed_unit_case1 = section.css('div.card-header h3::text').get(default='').strip() #jpm5, jpm8, jpm2
-            detailed_unit_case2 = section.css('div.card-header2 h4 b::text').get(default='').strip()
+            detailed_unit_case1 = section.css('div.card-header h3::text').get(default='').strip() #jpm5, jpm8, jpm2, and most jpm
+            detailed_unit_case2 = section.css('div.card-header2 h4 ::text').get(default='').strip() #jpm11
 
             if main_unit_case1:
                 main_unit = main_unit_case1
-            else:
+            elif main_unit_case2:
                 main_unit = main_unit_case2
+            else:
+                main_unit = None
 
             if detailed_unit_case1:
                 detailed_unit = detailed_unit_case1
-            else:
+            elif detailed_unit_case2:
                 detailed_unit = detailed_unit_case2
+            else:
+                detailed_unit = None
 
             if not main_unit and detailed_unit:
                 main_unit = detailed_unit
                 detailed_unit = None
+
+            # Handle cases where there might be only main_unit, only detailed_unit, both, or neither
+            unit_full = None
+            if main_unit and detailed_unit: #if both main unit and detailed unit exists
+                unit_full = f"{main_unit} > {detailed_unit}"  #combine main unit and detailed unit
+            elif detailed_unit:
+                unit_full = detailed_unit  #if only detailed_unit exists
+            elif main_unit:
+                unit_full = main_unit  #if only main_unit exists
 
             #iterate contact row in the section's table
             for contact in section.css('tbody tr.d-flex'):
@@ -71,20 +83,15 @@ class JPMSpider(scrapy.Spider):
                     person_email_prefix = ''     
                 person_email = f"{person_email_prefix}@jpm.gov.my" if person_email_prefix else ''
 
-                # Handle cases where there might be only main_unit, only detailed_unit, both, or neither
-                unit_full = None
-                if main_unit and detailed_unit:
-                    unit_full = f"{main_unit} > {detailed_unit}"  # Combine main unit and detailed unit
-                elif main_unit:
-                    unit_full = main_unit  # Only main unit exists
-
                 yield {
-                    'agency': agency,
+                    'agency': "JABATAN PERDANA MENTERI",
                     'division': division if division else None,
                     #'division_address': division_address,
                     #'division_phone': division_phone,
                     'person_name': person_name if person_name else None,
                     'unit': unit_full if unit_full else None,
+                    #'main_unit': main_unit, #debugging
+                    #'detailed_unit': detailed_unit, #debugging
                     'person_phone': person_phone if person_phone else None,
                     'person_position': person_position if person_position else None,
                     'person_email': person_email if person_email else None,
