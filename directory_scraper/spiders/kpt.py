ad5cb703a004ba32a52bc14d65b2102041af4b4c
AbdulHafiiz <arhafiiz@gmail.com>
2024-10-08 13:27:45 +0800

    changed scraping function from positional to explicit xpaths an added in additional error handling and cleaning

--- directory_scraper/spiders/kpt.py
+++ directory_scraper/spiders/kpt.py
@@ -75,69 +75,106 @@ class KPTSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow=r"/\d+?"), callback='parse_item'),
     )
+    
+    none_handler = lambda self, condition: result.strip() if (result := condition) else None
+    position_handler = lambda self, condition: result.replace("•", "").strip() if (result := condition) else None
+    sort_handler = lambda self, condition: result.strip()[:-1] if (result := condition) else None
 
     def parse_item(self, response):
-        clean_html = lambda string: re.sub(r"<.+?>|[\r\t\n]{2,}", "", string)
-        extract_number = lambda string: re.findall(pattern=r"03[\d\- ]+\d", string=string)
-        division = [text.strip() for line in response.css("div[class='card-body'] > div > div[class='col'] > b::text").getall() for text in line.split("\n") if text][0]
-        email_extension = clean_html(response.css("thead > tr > td > b").getall()[2]).split()[1]
-        department_number = extract_number(clean_html(response.css("div[class='alert alert-warning']").get()[0]))
-        default_unit = None
+        # clean_html = lambda string: re.sub(r"<.+?>|[\r\t\n]{2,}", "", string)
+        # extract_number = lambda string: re.findall(pattern=r"03[\d\- ]+\d", string=string)
+        # division = [text.strip() for line in response.css("div[class='card-body'] > div > div[class='col'] > b::text").getall() for text in line.split("\n") if text][0]
+        # email_extension = clean_html(response.css("thead > tr > td > b").getall()[2]).split()[1]
+        # department_number = extract_number(clean_html(response.css("div[class='alert alert-warning']").get()[0]))
+        # default_unit = None
 
-        for row_idx, row in enumerate(response.css("tbody > tr")):
-            person_data = {
-                "org_id": "KPT",
-                "org_name": "KEMENTERIAN PENDIDIKAN TINGGI",
-                "org_sort": 18,
-                "division_name": division,
-                "unit_name": default_unit,
-                "division_sort": self.directory_urls.index(response.url)+1
-            }
-            for idx, col in enumerate(row.css("td")):
-                if col.attrib.get("colspan") and col.attrib.get("valign"):
-                    default_unit = clean_html(col.css("b::text").get())
-                    print(default_unit)
-                    continue
+        current_unit = None
+        division = self.none_handler(response.css("div[class='col'] > b::text").get())
+        department_number = " ".join(response.css("div[class='alert alert-warning']::text").getall())
+        department_number = number[0] if (number := re.findall(r"0[\d -]+", department_number)) else None
+        email_extension = re.findall(r"@.*", response.xpath("//thead/tr/td[3]/b/text()").get())[0]
+        email_handler = lambda condition: f"{result}{email_extension}" if (result := condition) else None
 
-                temp_col = [i for i in clean_html(col.getall()[0]).split("\n") if len(i) > 1]
-                if idx == 0:
-                    person_data.update({
-                        "person_sort_order": temp_col.pop(0)[:-1] if temp_col else None
-                    })
-                elif idx == 1:
-                    # print(col)
-                    person_data.update({
-                        "person_name": temp_col.pop(0) if temp_col else None,
-                    })
-                elif idx == 2:
-                    person_data.update({
-                        "person_email": f"{temp_col.pop(0) if temp_col else None}{email_extension}"
-                    })
-                elif idx == 3:
-                    if department_number:
-                        extension_number = (f" ext {temp_col.pop(0).replace('*', '')}" if temp_col else "")
-                        phone_number = department_number + (extension_number or "")
-                        person_data.update({
-                            "person_phone": phone_number
-                        })
-                    else:
-                        person_data.update({
-                            "person_phone": f"{temp_col.pop(0) if temp_col else None}" 
-                        })
-            if len(person_data) > 5:
-                yield {
-                    "org_id": person_data.get("org_id"),
-                    "org_name": person_data.get("org_name"),
-                    "org_sort": person_data.get("org_sort"), 
+        for row in response.css("tbody > tr"):
+            if row.attrib:
+                continue
+            elif unit := row.css("b::text").get():
+                current_unit = unit
+            else:
+                phone_extension = self.none_handler(row.xpath("td[4]/text()").get())
+                person_data = {
+                    "org_id": "KPT",
+                    "org_name": "KEMENTERIAN PENDIDIKAN TINGGI",
+                    "org_sort": 18, 
                     "org_type": "ministry",
-                    "division_name": person_data.get("division_name"),
-                    "division_sort": person_data.get("division_sort"),
-                    "unit_name": person_data.get("unit_name"),
-                    "person_position": person_data.get("person_sort_order"),
-                    "person_name": person_data.get("person_name"),
-                    "person_email": person_data.get("person_email"),
+                    "division_name": division,
+                    "division_sort": self.directory_urls.index(response.url)+1,
+                    "unit_name": current_unit,
+                    "person_position": self.position_handler(row.xpath("td[2]/font/text()").get()),
+                    "person_name": self.none_handler(row.xpath("td[2]/text()").get()),
+                    "person_email": email_handler(row.xpath("td[3]/text()").get()),
                     "person_fax": None,
-                    "person_phone": person_data.get("person_phone"),
-                    "person_sort": person_data.get("person_sort_order"),
+                    "person_phone": self.none_handler((f"{department_number} " or "") + (f" ext {phone_extension}" or "")),
+                    "person_sort": self.sort_handler(row.xpath("td[1]/text()").get()),
                     "parent_org_id": None
                 }
+                yield person_data
+
+
+        # for row_idx, row in enumerate(response.css("tbody > tr")):
+        #     person_data = {
+        #         "org_id": "KPT",
+        #         "org_name": "KEMENTERIAN PENDIDIKAN TINGGI",
+        #         "org_sort": 18,
+        #         "division_name": division,
+        #         "unit_name": default_unit,
+        #         "division_sort": self.directory_urls.index(response.url)+1
+        #     }
+        #     for idx, col in enumerate(row.css("td")):
+        #         if col.attrib.get("colspan") and col.attrib.get("valign"):
+        #             default_unit = clean_html(col.css("b::text").get())
+        #             print(default_unit)
+        #             continue
+
+        #         temp_col = [i for i in clean_html(col.getall()[0]).split("\n") if len(i) > 1]
+        #         if idx == 0:
+        #             person_data.update({
+        #                 "person_sort_order": temp_col.pop(0)[:-1] if temp_col else None
+        #             })
+        #         elif idx == 1:
+        #             # print(col)
+        #             person_data.update({
+        #                 "person_name": temp_col.pop(0) if temp_col else None,
+        #             })
+        #         elif idx == 2:
+        #             person_data.update({
+        #                 "person_email": f"{temp_col.pop(0) if temp_col else None}{email_extension}"
+        #             })
+        #         elif idx == 3:
+        #             if department_number:
+        #                 extension_number = (f" ext {temp_col.pop(0).replace('*', '')}" if temp_col else "")
+        #                 phone_number = department_number + (extension_number or "")
+        #                 person_data.update({
+        #                     "person_phone": phone_number
+        #                 })
+        #             else:
+        #                 person_data.update({
+        #                     "person_phone": f"{temp_col.pop(0) if temp_col else None}" 
+        #                 })
+        #     if len(person_data) > 5:
+        #         yield {
+        #             "org_id": person_data.get("org_id"),
+        #             "org_name": person_data.get("org_name"),
+        #             "org_sort": person_data.get("org_sort"), 
+        #             "org_type": "ministry",
+        #             "division_name": person_data.get("division_name"),
+        #             "division_sort": person_data.get("division_sort"),
+        #             "unit_name": person_data.get("unit_name"),
+        #             "person_position": person_data.get("person_sort_order"),
+        #             "person_name": person_data.get("person_name"),
+        #             "person_email": person_data.get("person_email"),
+        #             "person_fax": None,
+        #             "person_phone": person_data.get("person_phone"),
+        #             "person_sort": person_data.get("person_sort_order"),
+        #             "parent_org_id": None
+        #         }
