b3f2fea8847856b120dc185edd814440000352cb
AbdulHafiiz <arhafiiz@gmail.com>
2024-10-15 15:19:52 +0800

    removed comments for division_mappings

--- directory_scraper/spiders/moh.py
+++ directory_scraper/spiders/moh.py
@@ -10,35 +10,35 @@ class MOHSpider(scrapy.Spider):
     none_handler = lambda self, condition: result if (result := condition) else None
     
     division_mapping = [
-        # {"division_name": "Bahagian Akaun", "division_code": "29", "division_sort_order": 39},
-        # {"division_name": "Bahagian Amalan Perubatan", "division_code": "2", "division_sort_order": 26},
-        # {"division_name": "Bahagian Dasar Dan Hubungan Antarabangsa", "division_code": "30", "division_sort_order": 40},
-        # {"division_name": "Bahagian Kawalan Penyakit", "division_code": "3", "division_sort_order": 27},
-        # {"division_name": "Bahagian Kawalselia Radiasi Perubatan", "division_code": "86", "division_sort_order": 22},
-        # {"division_name": "Bahagian Kejururawatan", "division_code": "13", "division_sort_order": 36},
-        # {"division_name": "Bahagian Kewangan", "division_code": "31", "division_sort_order": 41},
-        # {"division_name": "Bahagian Khidmat Pengurusan", "division_code": "7", "division_sort_order": 31},
-        # {"division_name": "Bahagian Pemakanan", "division_code": "72", "division_sort_order": 16},
-        # {"division_name": "Bahagian Pembangunan", "division_code": "81", "division_sort_order": 19},
-        # {"division_name": "Bahagian Pembangunan Kesihatan Keluarga", "division_code": "8", "division_sort_order": 32},
-        # {"division_name": "Bahagian Pembangunan Kompetensi", "division_code": "32", "division_sort_order": 18},
-        # {"division_name": "Bahagian Pendidikan Kesihatan", "division_code": "9", "division_sort_order": 33},
-        # {"division_name": "Bahagian Pengurusan Latihan", "division_code": "33", "division_sort_order": 11},
-        # {"division_name": "Bahagian Pengurusan Maklumat", "division_code": "12", "division_sort_order": 35},
-        # {"division_name": "Bahagian Perancangan", "division_code": "80", "division_sort_order": 10},
-        # {"division_name": "Bahagian Perkembangan Kesihatan Awam", "division_code": "85", "division_sort_order": 21},
-        # {"division_name": "Bahagian Perkembangan Perubatan", "division_code": "11", "division_sort_order": 25},
-        # {"division_name": "Bahagian Perkhidmatan Kejuruteraan", "division_code": "4", "division_sort_order": 28},
-        # {"division_name": "Bahagian Perolehan Dan Penswastaan", "division_code": "34", "division_sort_order": 12},
-        # {"division_name": "Bahagian Perubatan Tradisional Dan Komplementari", "division_code": "35", "division_sort_order": 13},
-        # {"division_name": "Bahagian Sains Kesihatan Bersekutu", "division_code": "74", "division_sort_order": 17},
-        # {"division_name": "Bahagian Sumber Manusia", "division_code": "36", "division_sort_order": 14},
-        # {"division_name": "Cawangan Audit Dalam", "division_code": "1", "division_sort_order": 34},
-        # {"division_name": "Health Performance Unit (HPU)", "division_code": "124", "division_sort_order": 23},
-        # {"division_name": "Pejabat Ketua Pengarah Kesihatan", "division_code": "17", "division_sort_order": 6},
-        # {"division_name": "Pejabat Ketua Setiausaha", "division_code": "18", "division_sort_order": 3},
-        # {"division_name": "Pejabat Menteri Kesihatan", "division_code": "19", "division_sort_order": 1},
-        # {"division_name": "Pejabat Penasihat Undang-Undang", "division_code": "20", "division_sort_order": 37},
+        {"division_name": "Bahagian Akaun", "division_code": "29", "division_sort_order": 39},
+        {"division_name": "Bahagian Amalan Perubatan", "division_code": "2", "division_sort_order": 26},
+        {"division_name": "Bahagian Dasar Dan Hubungan Antarabangsa", "division_code": "30", "division_sort_order": 40},
+        {"division_name": "Bahagian Kawalan Penyakit", "division_code": "3", "division_sort_order": 27},
+        {"division_name": "Bahagian Kawalselia Radiasi Perubatan", "division_code": "86", "division_sort_order": 22},
+        {"division_name": "Bahagian Kejururawatan", "division_code": "13", "division_sort_order": 36},
+        {"division_name": "Bahagian Kewangan", "division_code": "31", "division_sort_order": 41},
+        {"division_name": "Bahagian Khidmat Pengurusan", "division_code": "7", "division_sort_order": 31},
+        {"division_name": "Bahagian Pemakanan", "division_code": "72", "division_sort_order": 16},
+        {"division_name": "Bahagian Pembangunan", "division_code": "81", "division_sort_order": 19},
+        {"division_name": "Bahagian Pembangunan Kesihatan Keluarga", "division_code": "8", "division_sort_order": 32},
+        {"division_name": "Bahagian Pembangunan Kompetensi", "division_code": "32", "division_sort_order": 18},
+        {"division_name": "Bahagian Pendidikan Kesihatan", "division_code": "9", "division_sort_order": 33},
+        {"division_name": "Bahagian Pengurusan Latihan", "division_code": "33", "division_sort_order": 11},
+        {"division_name": "Bahagian Pengurusan Maklumat", "division_code": "12", "division_sort_order": 35},
+        {"division_name": "Bahagian Perancangan", "division_code": "80", "division_sort_order": 10},
+        {"division_name": "Bahagian Perkembangan Kesihatan Awam", "division_code": "85", "division_sort_order": 21},
+        {"division_name": "Bahagian Perkembangan Perubatan", "division_code": "11", "division_sort_order": 25},
+        {"division_name": "Bahagian Perkhidmatan Kejuruteraan", "division_code": "4", "division_sort_order": 28},
+        {"division_name": "Bahagian Perolehan Dan Penswastaan", "division_code": "34", "division_sort_order": 12},
+        {"division_name": "Bahagian Perubatan Tradisional Dan Komplementari", "division_code": "35", "division_sort_order": 13},
+        {"division_name": "Bahagian Sains Kesihatan Bersekutu", "division_code": "74", "division_sort_order": 17},
+        {"division_name": "Bahagian Sumber Manusia", "division_code": "36", "division_sort_order": 14},
+        {"division_name": "Cawangan Audit Dalam", "division_code": "1", "division_sort_order": 34},
+        {"division_name": "Health Performance Unit (HPU)", "division_code": "124", "division_sort_order": 23},
+        {"division_name": "Pejabat Ketua Pengarah Kesihatan", "division_code": "17", "division_sort_order": 6},
+        {"division_name": "Pejabat Ketua Setiausaha", "division_code": "18", "division_sort_order": 3},
+        {"division_name": "Pejabat Menteri Kesihatan", "division_code": "19", "division_sort_order": 1},
+        {"division_name": "Pejabat Penasihat Undang-Undang", "division_code": "20", "division_sort_order": 37},
         {"division_name": "Pejabat Timbalan Ketua Pengarah Kesihatan (Kesihatan Awam)", "division_code": "63", "division_sort_order": 8},
         {"division_name": "Pejabat Timbalan Ketua Pengarah Kesihatan (Penyelidikan & Sokongan Teknikal)", "division_code": "64", "division_sort_order": 9},
         {"division_name": "Pejabat Timbalan Ketua Pengarah Kesihatan (Perubatan)", "division_code": "24", "division_sort_order": 7},
@@ -110,7 +110,7 @@ class MOHSpider(scrapy.Spider):
             next_page_available = current_page.css("a[class='paginate_button next']")
             
             if next_page_available:
-                print(f"\n\nNEXT PAGE AVAILABLE: {response.meta["page_number"]}")
+                # print(f"\n\nNEXT PAGE AVAILABLE: {response.meta["page_number"]}")
                 await page.click("a[class='paginate_button next']")
                 
                 response.meta["page_number"] += 1
@@ -121,5 +121,5 @@ class MOHSpider(scrapy.Spider):
                     continue
             else:
                 break
-        print(f"\n\nCLOSING PAGE: {response.url}")
+        # print(f"\n\nCLOSING PAGE: {response.url}")
         await page.close()
