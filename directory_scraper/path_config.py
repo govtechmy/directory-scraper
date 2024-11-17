a53f36c7eb21dc7615d5d721a50c4c78981c2a92
Asyiqin Nasir <asyiq@MacBook-Pro-2.local>
2024-11-17 17:25:17 +0800

    chore: path_config.py - removed ES conf

--- directory_scraper/path_config.py
+++ directory_scraper/path_config.py
@@ -7,8 +7,3 @@ DEFAULT_SPIDERS_OUTPUT_FOLDER = "data/spiders_output"
 DEFAULT_CLEAN_DATA_FOLDER = "data/clean_data"
 DEFAULT_LOG_DIR = "logs"
 DEFAULT_BACKUP_FOLDER = "data/backups"
-
-# Elastic Search
-ES_URL = "http://localhost:9200"
-INDEX_NAME = "directory"
-SHA_INDEX_NAME = "directory-sha"
