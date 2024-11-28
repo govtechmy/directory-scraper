9ecd4bfcec245a837ec46c2346aeeb62a9a9103c
AbdulHafiiz <arhafiiz@gmail.com>
2024-11-28 16:19:16 +0800

    initial commit for docker containerization of fastapi backend

---
+++ directory_scraper/api_setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+setup_dir = os.path.dirname(__file__)
+requirements_path = os.path.join(setup_dir, 'requirements.txt')
+
+with open(requirements_path) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='directory_scraper_api',               # Name of your project
+    version='0.1',                   # Version of your project
+    packages=find_packages('directory_scraper'),   # Look for packages in the 'src' directory
+    package_dir={'': 'directory_scraper'},         # Root package directory is 'src'
+    install_requires=requirements,      # Set dependencies from requirements.txt
+)
