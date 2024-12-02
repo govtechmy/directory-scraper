9a9777d019391c80534a255c2ac5d2fcf25660d6
Asyiqin Nasir <asyiq@MacBook-Pro-2.local>
2024-12-02 18:56:06 +0800

    chore: uncommented list of spiders

--- directory_scraper/src/data_processing/run_spiders.py
+++ directory_scraper/src/data_processing/run_spiders.py
@@ -583,7 +583,7 @@ def main(spider_list=None, output_folder=None, backup_folder=None):
         all_spiders = get_all_spiders()
         logger.debug(f"Spider tree: {spider_tree}")
 
-        LIST_OF_SPIDERS_TO_RUN = ['digital', 'ekonomi', 'kbs', 'kkr', 'kln', 'komunikasi', 'kpdn', 'kpk', 'kpkm', 'kpkt', 'kpn', 'kpt', 'kpwkm', 'kuskop', 'miti', 'mof', 'moh', 'moha', 'mosti', 'mot', 'motac', 'nres', 'petra', 'rurallink_anggota', 'rurallink_pkd'] #['jpm', 'mohr', 'mod', 'moe',]
+        LIST_OF_SPIDERS_TO_RUN = ['digital', 'ekonomi', 'kbs', 'kkr', 'kln', 'komunikasi', 'kpdn', 'kpk', 'kpkm', 'kpkt', 'kpn', 'kpt', 'kpwkm', 'kuskop', 'miti', 'mof', 'moh', 'moha', 'mosti', 'mot', 'motac', 'nres', 'petra', 'rurallink_anggota', 'rurallink_pkd', 'jpm', 'mohr', 'mod', 'moe',]
 
         #================== ARGS VALIDATION =====================
         if not validate_arg_name(args.name, all_spiders, spider_tree):
