6d110339b3cf00515f82f06a9390038c05e3fdf0
AbdulHafiiz <arhafiiz@gmail.com>
2024-12-26 20:58:23 +0800

    Feat: - posSort function:     - Added in condition where default cell formulas would only be populated after the second row - onChange function     - Added in checks to ensure that onChange trigger wont be recreated when rerunning the function - onEdit function:     - Added in condition to the function onEdit to only occur from the second row onwards     - Reworked position_sort formula to be empty if the other data cells are empty - createValidation function:     - protected data ranges to prevent users tampering with the formulas     - changed org id array from a hardcoded static list to a dynamic gsheet range - mainSetup function:     - moved non-function-like code into separate function (setupLookupSheet) to preserve tidyness     - now calls other functions instead of executing is own code     - added in logging for all stages - setupLookupSheet function:     - reworked sheetOrgId to deal with mixed-case titles     - replaced IMPORTRANGE gsheet functions with dynamically extracted data from an external master reference sheet - columnConversion function:     - added in new function to convert previous textual data from org_sort, org_id, org_type, division_sort and position_sort into gsheet functions

--- sheet_scripts/ministry-template/main.js
+++ sheet_scripts/ministry-template/main.js
@@ -6,17 +6,19 @@ function posSort(e) {
   if (sheet.getName() != "LookupSheet") {
     if(e.changeType === 'INSERT_ROW') {
       var row = sheet.getActiveRange().getRow();
-      var orgSortFormula = "=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$B$2, 'Value not found')";
-      var orgIdFormula = "=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$A$2, 'Value not found)')";
-      var orgTypeFormula = "=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$C$2, 'Value not found)')";
-      var divSortFormula = "=XLOOKUP(INDIRECT(ADDRESS(ROW(),6)),LookupSheet!$G$2:$G,LookupSheet!$F$2:$F, 'Value not found')";
-      var posSortFormula = "=IF(INDIRECT(ADDRESS(ROW()-1, 5))=INDIRECT(ADDRESS(ROW(),5)), INDIRECT(ADDRESS(ROW()-1,8))+1, 1)";
+      if (row > 1){
+        var orgSortFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$B$2, "")';
+        var orgIdFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$A$2, "")';
+        var orgTypeFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$C$2, "")';
+        var divSortFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(),6)),LookupSheet!$F$2:$F,LookupSheet!$E$2:$E, "")';
+        var posSortFormula = '=IF(AND(ARRAYFORMULA(OFFSET(INDIRECT(ADDRESS(ROW(), COLUMN())), 0, -7, 1, 7)="")),"",IF(INDIRECT(ADDRESS(ROW()-1, 5))=INDIRECT(ADDRESS(ROW(),5)), INDIRECT(ADDRESS(ROW()-1,8))+1, 1))';
 
-      sheet.getRange(row, 1).setValue(orgSortFormula);
-      sheet.getRange(row, 2).setValue(orgIdFormula);
-      sheet.getRange(row, 4).setValue(orgTypeFormula);
-      sheet.getRange(row, 5).setValue(divSortFormula);
-      sheet.getRange(row, 8).setValue(posSortFormula);
+        sheet.getRange(row, 1).setValue(orgSortFormula);
+        sheet.getRange(row, 2).setValue(orgIdFormula);
+        sheet.getRange(row, 4).setValue(orgTypeFormula);
+        sheet.getRange(row, 5).setValue(divSortFormula);
+        sheet.getRange(row, 8).setValue(posSortFormula);
+      }
     }
   }
 }
@@ -25,10 +27,23 @@ function posSort(e) {
  * Automatically create spreadsheet trigger
  */
 function createSpreadsheetChangeTrigger() {
-  ScriptApp.newTrigger('onChange')
-    .forSpreadsheet(ss)
+  var changeTriggerExists = false;
+  var allTriggers = ScriptApp.getProjectTriggers();
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  for (let i=0;i<allTriggers.length;i++){
+    if (allTriggers[i].getHandlerFunction()=="posSort") {
+      changeTriggerExists = true;
+    }
+  }
+
+  if (changeTriggerExists) {
+    console.log("Trigger already exists, skipping step");
+  } else {
+    ScriptApp.newTrigger('posSort')
+    .forSpreadsheet(spreadsheet)
     .onChange()
     .create();
+  }
 }
 
 function onEdit(e) {
@@ -36,13 +51,15 @@ function onEdit(e) {
   var sheetName = sheet.getName();
   if (sheetName != "LookupSheet") {
     var rowStart = e.range.rowStart;
-    var rowCount = e.range.rowEnd - rowStart + 1;
-    var sortRange = sheet.getRange(rowStart, 8, rowCount, 1);
-    var dataRowLeft = sheet.getRange(rowStart, 1, rowCount, 7);
-    var dataRowRight = sheet.getRange(rowStart, 9, rowCount, 7);
+    if (rowStart > 1){
+      var rowCount = e.range.rowEnd - rowStart + 1;
+      var sortRange = sheet.getRange(rowStart, 8, rowCount, 1);
+      var dataRowLeft = sheet.getRange(rowStart, 1, rowCount, 7);
+      var dataRowRight = sheet.getRange(rowStart, 9, rowCount, 7);
 
-    if (!dataRowLeft.isBlank() && !dataRowRight.isBlank()) {
-      sortRange.setFormula("=IF(INDIRECT(ADDRESS(ROW()-1, 5))=INDIRECT(ADDRESS(ROW(),5)),INDIRECT(ADDRESS(ROW()-1,8))+1,1)");
+      if (!dataRowLeft.isBlank() && !dataRowRight.isBlank()) {
+        sortRange.setFormula('=IF(AND(ARRAYFORMULA(OFFSET(INDIRECT(ADDRESS(ROW(), COLUMN())), 0, -7, 1, 7)="")),"",IF(INDIRECT(ADDRESS(ROW()-1, 5))=INDIRECT(ADDRESS(ROW(),5)), INDIRECT(ADDRESS(ROW()-1,8))+1, 1))');
+      }
     }
   }
 }
@@ -53,43 +70,46 @@ function onEdit(e) {
 function createValidation() {
   var dataSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetById(0);
   var dataRefSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("LookupSheet");
-  var orgRefSheet = SpreadsheetApp.openById("11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw").getSheetById(0);
-  var lastRowRefSheet = orgRefSheet.getLastRow();
+  var lastRowRefSheet = dataRefSheet.getLastRow();
   
   // org_sort data validation
   var orgSortRule = SpreadsheetApp.newDataValidation().requireNumberBetween(1, 29).build();
   var orgSortRange = dataSheet.getRange("$A$2:$A");
-  orgSortRange.setDataValidation(orgSortRule)
+  orgSortRange.setDataValidation(orgSortRule);
+  orgSortRange.protect().setDescription("The sorting order of the Organisation");
 
   // org_id data validation
-  var orgIdRule = SpreadsheetApp.newDataValidation().requireValueInList([
-    "JPM", "MOF", "RURALLINK", "PETRA", "MOT", "KPKM", "EKONOMI", "KPKT", "KLN",
-    "KKR", "MOHA", "MITI", "MOD", "MOSTI", "KPWKM", "NRES", "KUSKOP", "KPT", "MOTAC",
-    "KOMUNIKASI", "MOE", "KPN", "KBS", "JWP", "KPDN", "KPK", "DIGITAL", "MOH", "MOHR"], true).build();
+  var orgIdArray = dataRefSheet.getRange("$A$2:$A").getValues().filter(String);
+  var orgIdRule = SpreadsheetApp.newDataValidation().requireValueInList(orgIdArray, false).build();
   var orgIdRange = dataSheet.getRange("Sheet1!$B$2:$B");
   orgIdRange.setDataValidation(orgIdRule);
+  orgIdRange.protect().setDescription("The ID of the Organisation");
 
   // org_name data validation
-  var orgArray = dataRefSheet.getRange("$D$2:$D$29").getValues();
-  var orgNameRule = SpreadsheetApp.newDataValidation().requireValueInList(orgArray);
+  var orgArray = dataRefSheet.getRange("$D$2:$D").getValues().filter(String);
+  var orgNameRule = SpreadsheetApp.newDataValidation().requireValueInList(orgArray).build();
   var orgNameRange = dataSheet.getRange("$C$2:$C");
   orgNameRange.setDataValidation(orgNameRule);
+  orgNameRange.protect().setDescription("The full name of the Organisation");
 
   // org_type data validation
   var orgTypeRule = SpreadsheetApp.newDataValidation().requireValueInList(["ministry"], false).build();
   var orgTypeRange = dataSheet.getRange("$D$2:$D")
   orgTypeRange.setDataValidation(orgTypeRule);
+  orgTypeRange.protect().setDescription("The type of the Organisation");
 
   // division_sort data validation
   var divSortRule = SpreadsheetApp.newDataValidation().requireNumberGreaterThan(0).build();
   var divSortRange = dataSheet.getRange("$E$2:$E")
   divSortRange.setDataValidation(divSortRule)
+  divSortRange.protect().setDescription("The sort order of the Organisation's divisions");
 
   // division_name data validation
   var divArray = dataRefSheet.getRange(2, 6, lastRowRefSheet-1, 1).getValues().flat();
   var divNameRule = SpreadsheetApp.newDataValidation().requireValueInList(divArray, true).build();
   var divNameRange = dataSheet.getRange("$F$2:F");
   divNameRange.setDataValidation(divNameRule);
+  divNameRange.protect().setDescription("The division names under the Organisation");
 
   // person_email data validation
   var emailRange = dataSheet.getRange("$K$2:$K");
@@ -98,23 +118,75 @@ function createValidation() {
 }
 
 /**
- * Main setup function for the google sheet
+ * Function to setup lookup sheet
  */
-function mainSetup() {
+function setupLookupSheet() {
   var spreadsheets = SpreadsheetApp.getActiveSpreadsheet();
   var sheetName = spreadsheets.getName();
-  var sheetOrgId = sheetName.split(" ").pop();
+  var sheetOrgId = sheetName.split(" ").pop().toUpperCase();
   var lookupSheet = spreadsheets.getSheetByName("LookupSheet");
+  var refSheet = SpreadsheetApp.openById("11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw")
 
   if (lookupSheet == null) {
     lookupSheet = spreadsheets.insertSheet("LookupSheet");
     lookupSheet.appendRow(["org_id", "org_sort", "org_type", "org_name", "division_sort", "division_name"]);
   }
-  lookupSheet.getRange("A2").setValue(sheetOrgId);
 
-  lookupSheet.getRange("B2").setFormula('=FILTER(IMPORTRANGE("https://docs.google.com/spreadsheets/d/11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw/edit?gid=0#gid=0", "OrgLookup!$B$2:$D$29"), IMPORTRANGE("https://docs.google.com/spreadsheets/d/11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw/edit?gid=0#gid=0", "OrgLookup!$A$2:$A$29")=$A$2)');
+  var orgSheet = refSheet.getSheetByName("OrgLookup");
+  var rowCountOrg = orgSheet.getDataRange().getLastRow()-1;
+  var orgData = orgSheet
+  .getRange(2, 1, rowCountOrg, 4)
+  .getValues()
+  .filter(function (x) {return (x[0]==sheetOrgId)});
 
-  lookupSheet.getRange("H2").setFormula('=FILTER(IMPORTRANGE("https://docs.google.com/spreadsheets/d/11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw/edit?gid=325480181#gid=325480181", "DivisionLookup!$B$2:$C"), IMPORTRANGE("https://docs.google.com/spreadsheets/d/11iszULwAgixtSgSfUmyQt4EX054CTe47xMn4QheVgJw/edit?gid=325480181#gid=325480181", "DivisionLookup!A2:A")=$D$2)');
+  var divSheet = refSheet.getSheetByName("DivisionLookup");
+  var rowCountDiv = divSheet.getDataRange().getLastRow()-1;
+  var divData = refSheet.getSheetByName("DivisionLookup")
+  .getRange(2, 1, rowCountDiv, 4)
+  .getValues()
+  .filter(function (x) {return (x[0]==sheetOrgId)})
+  .map(function (x) {return x.slice(2)});
 
+
+  lookupSheet.getRange("$A$2:$D$2").setValues(orgData);
+  lookupSheet.getRange(2, 5, divData.length, 2).setValues(divData);
+}
+
+/**
+ * Converting columns org_sort, org_id, org_type, div_sort, pos_sort into gsheet formulas
+ */
+function columnConversion() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetById(0);
+  var orgSortFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$B$2, "")';
+  var orgIdFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$A$2, "")';
+  var orgTypeFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(), 3)),LookupSheet!$D$2,LookupSheet!$C$2, "")';
+  var divSortFormula = '=XLOOKUP(INDIRECT(ADDRESS(ROW(),6)),LookupSheet!$F$2:$F,LookupSheet!$E$2:$E, "")';
+  var posSortFormula = '=IF(AND(ARRAYFORMULA(OFFSET(INDIRECT(ADDRESS(ROW(), COLUMN())), 0, -7, 1, 7)="")),"",IF(INDIRECT(ADDRESS(ROW()-1, 5))=INDIRECT(ADDRESS(ROW(),5)), INDIRECT(ADDRESS(ROW()-1,8))+1, 1))';
+
+  sheet.getRange("$A$2:$A").setValue(orgSortFormula);
+  sheet.getRange("$B$2:$B").setValue(orgIdFormula);
+  sheet.getRange("$D$2:$D").setValue(orgTypeFormula);
+  sheet.getRange("$E$2:$E").setValue(divSortFormula);
+  sheet.getRange("$H$2:$H").setValue(posSortFormula);
+}
+
+/**
+ * Main setup function for the google sheet
+ */
+function mainSetup() {
+  console.log("Setting Lookup Sheet");
+  setupLookupSheet();
+  console.log("Successfully set up Lookup Sheet");
+
+  console.log("Converting prepopulated columns to XLOOKUP formulas");
+  columnConversion();
+  console.log("Finished column conversion");
+
+  console.log("Creating data validation rules");
   createValidation();
+  console.log("Successfully created validation rules");
+
+  console.log("Creating new row spreadsheet trigger");
+  createSpreadsheetChangeTrigger();
+  console.log("Spreadsheet trigger successfully created");
 }
