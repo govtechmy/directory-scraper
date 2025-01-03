4a4be37c6af5319eb339147cbae63c29ff840cba
AbdulHafiiz <arhafiiz@gmail.com>
2025-01-03 10:54:21 +0800

    feat: now logs clasp action types and now propagates appsscript.json files from template to deployed apps

--- sheet_scripts/gas_ministry_setup.sh
+++ sheet_scripts/gas_ministry_setup.sh
@@ -23,7 +23,7 @@ echo "Creating folders: ${SHEET_NAMES[@]}"
 max_jobs=25
 poll_delay="0.1s"
 
-declare -A pid_pro_map=() pid_log_map=()
+declare -A pid_pro_map=() pid_log_map=() pid_type_map=()
 for name in "${SHEET_NAMES[@]}"; do # Loops over array, creates directory with the array names and copies main.js into the new directories
     # Sleeps if the number of current processes exceedes the max_jobs value
     if (( ${max_jobs} > 0 )); then
@@ -35,30 +35,39 @@ for name in "${SHEET_NAMES[@]}"; do # Loops over array, creates directory with t
     # Creates and moves into the directory
     mkdir -p "${BASE_DIR}/deployments/${name}" && cd "${BASE_DIR}/deployments/${name}"
     cp "${BASE_DIR}/ministry-template/main.js" "${BASE_DIR}/deployments/${name}/main.js"
+    cp "${BASE_DIR}/ministry-template/appsscript.json" "${BASE_DIR}/deployments/${name}/appsscript.json"
 
     logfile=$(mktemp -p "${logdir}")
+    pid_lst=()
     # If project exists, skip the creation step and push the changes to remote
     if [ -e '.clasp.json' ]; then
-        echo "Pushing code to sheet: Directory Gov - ${name}"
-        clasp push -f && clasp push &> "${logfile}" &
+        # echo "Pushing code to sheet: Directory Gov - ${name}"
+        clasp push -f  &> /dev/null & pid_lst+=( "$!" );
+        pid_type_map+=( "push" );
+        pid_type_map["$!"]='push'
+        clasp push  &> /dev/null & pid_lst+=( "$!" );
+        pid_type_map+=( "push" )
+        pid_type_map["$!"]='push'
     else
         # Runs clasp action and pipes the output to a temporary logfile
-        (clasp create --type sheets --title "Directory Gov - ${name}") &> "${logfile}" &
-        clasp push && clasp push # Double push to forcefully update remote
+        (clasp create --type sheets --title "Directory Gov - ${name}") &> "${logfile}" & pid_lst+=( "$!" ); pid_type_map+=( "create" )
+        # Double push to forcefully update remote
+        clasp push -f & pid_lst+=( "$!" ); pid_type_map+=( "push" )
+        clasp push & pid_lst+=( "$!" ); pid_type_map+=( "push" )
 
     fi
-    # Deploy the GAS project
-    clasp deploy --versionNumber $CODE_VERSION --description 'Initial version' &> "${logfile}" &
 
     # Takes the PID of the last program run in the shell and keeps them in mapps them to the sheet name and the logfile
-    pid=$!; pid_pro_map[${pid}]="${name}"; pid_log_map[${pid}]="${logfile}"
-    echo -e "Started clasp action for project '\e[1m${name}\e[0m' (pid ${pid})"
+    for pid in "${pid_lst[@]}"; do
+        pid_pro_map[$pid]="${name}"; pid_log_map[$pid]="${logfile}"
+    done
+    # echo -e "Started clasp action for project '\e[1m${name}\e[0m' (pid ${pid_lst[@]})"
 
 done
 
 # Wait for background jobs to finish and report results
 echo -e "\nWaiting for background jobs to finish...\n"
-jobs_done=0; jobs_total=${#SHEET_NAMES[@]}
+jobs_done=0; jobs_total=${#pid_pro_map[@]}
 while true; do
     # Waits for PID to return exit code and assigns it to the variable `results`
     wait -n -p pid; result=$?
@@ -68,9 +77,9 @@ while true; do
 
     # Echos out exit code of processes
     if (( ${result} == 0 )); then
-        echo -e "Clasp action for project '\e[1m${pid_pro_map[${pid}]}\e[0m' (pid ${pid}) (${jobs_done}/${jobs_total}): \e[1;32mSUCCESS\e[0m"
+        echo -e "Clasp action for project '\e[1m${pid_pro_map[${pid}]}\e[0m' (action ${pid_type_map[${pid}]}) (pid ${pid}) (${jobs_done}/${jobs_total}): \e[1;32mSUCCESS\e[0m"
     else
-        echo -e "Clasp action for project '\e[1m${pid_pro_map[${pid}]}\e[0m' (pid ${pid}) (${jobs_done}/${jobs_total}): \e[1;31mFAILURE\e[0m"
+        echo -e "Clasp action for project '\e[1m${pid_pro_map[${pid}]}\e[0m' (action ${pid_type_map[${pid}]}) (pid ${pid}) (${jobs_done}/${jobs_total}): \e[1;31mFAILURE\e[0m"
         cat "${pid_log_map[${pid}]}"
     fi
 done
